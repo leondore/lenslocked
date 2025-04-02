971425f4c90f4e62137e81bf191eefc72adf24ff
Leon Dore <leon.dore@gmail.com>
2025-04-02 19:31:40 -0400

    multiple pattern support to ParseFS function

--- views/template.go
+++ views/template.go
@@ -8,8 +8,8 @@ import (
 	"net/http"
 )
 
-func ParseFS(fs fs.FS, pattern string) (Template, error) {
-	tpl, err := template.ParseFS(fs, pattern)
+func ParseFS(fs fs.FS, patterns ...string) (Template, error) {
+	tpl, err := template.ParseFS(fs, patterns...)
 	if err != nil {
 		return Template{}, fmt.Errorf("parsing template: %w", err)
 	}
