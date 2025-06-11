6c7118e3e76b1261e6a06c9bf6c700a413442b6d
Leon Dore <leon.dore@gmail.com>
2025-06-10 21:02:17 -0400

    create user service

--- models/user.go
+++ models/user.go
@@ -1,7 +1,49 @@
 package models
 
+import (
+	"database/sql"
+	"fmt"
+	"strings"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
 type User struct {
 	ID           int
 	Email        string
 	PasswordHash string
+}
+
+type NewUser struct {
+	Email    string
+	Password string
+}
+
+type UserService struct {
+	DB *sql.DB
+}
+
+func (us *UserService) Create(nu NewUser) (*User, error) {
+	hashedPwBytes, err := bcrypt.GenerateFromPassword([]byte(nu.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("create user: %w", err)
+	}
+	hashedPassword := string(hashedPwBytes)
+	email := strings.ToLower(nu.Email)
+
+	user := User{
+		Email:        email,
+		PasswordHash: hashedPassword,
+	}
+
+	row := us.DB.QueryRow(`
+		INSERT INTO users (email, password_hash)
+		VALUES ($1, $2) RETURNING id;`, email, hashedPassword,
+	)
+	err = row.Scan(&user.ID)
+	if err != nil {
+		return nil, fmt.Errorf("create user: %w", err)
+	}
+
+	return &user, nil
 }
