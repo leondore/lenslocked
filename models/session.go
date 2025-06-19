566eecc4cfd4e3f7c1f473e64d3943fc2ae2de3a
Leon Dore <leon.dore@gmail.com>
2025-06-18 23:26:18 -0400

    hash session tokens

--- models/session.go
+++ models/session.go
@@ -1,7 +1,9 @@
 package models
 
 import (
+	"crypto/sha256"
 	"database/sql"
+	"encoding/base64"
 	"fmt"
 
 	"github.com/leondore/lenslocked/rand"
@@ -32,8 +34,9 @@ func (ss *SessionService) Create(userID int) (*Session, error) {
 	}
 
 	session := Session{
-		UserID: userID,
-		Token:  token,
+		UserID:    userID,
+		Token:     token,
+		TokenHash: ss.hash(token),
 	}
 
 	return &session, nil
@@ -41,4 +44,9 @@ func (ss *SessionService) Create(userID int) (*Session, error) {
 
 func (ss *SessionService) User(token string) (*User, error) {
 	return nil, nil
+}
+
+func (ss *SessionService) hash(token string) string {
+	tokenHash := sha256.Sum256([]byte(token))
+	return base64.URLEncoding.EncodeToString(tokenHash[:])
 }
