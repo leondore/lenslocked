3169684acc2b566c126c49f0e8d33b2c34dd8f3c
Leon Dore <leon.dore@gmail.com>
2025-06-28 20:30:33 -0400

    using join in the sessionservice

--- models/session.go
+++ models/session.go
@@ -64,19 +64,11 @@ func (ss *SessionService) User(token string) (*User, error) {
 
 	var user User
 	row := ss.DB.QueryRow(`
-		SELECT user_id
+		SELECT users.id, users.email, users.password_hash
 		FROM sessions
-		WHERE token_hash = $1;`, tokenHash)
-	err := row.Scan(&user.ID)
-	if err != nil {
-		return nil, fmt.Errorf("user from session: %w", err)
-	}
-
-	row = ss.DB.QueryRow(`
-		SELECT email, password_hash
-		FROM users
-		WHERE id = $1;`, user.ID)
-	err = row.Scan(&user.Email, &user.PasswordHash)
+		JOIN users ON users.id = sessions.user_id
+		WHERE sessions.token_hash = $1;`, tokenHash)
+	err := row.Scan(&user.ID, &user.Email, &user.PasswordHash)
 	if err != nil {
 		return nil, fmt.Errorf("user from session: %w", err)
 	}
